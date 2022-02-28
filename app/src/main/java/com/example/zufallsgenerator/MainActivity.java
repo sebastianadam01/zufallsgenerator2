2c09c9fa2562d0e00aba781a325c6cd0da831a89
Christian <christian.zeiler002@gmx.at>
2022-02-28 14:51:11 +0100

    Meine Kommentarzeile!!!

--- app/src/main/java/com/example/zufallsgenerator/MainActivity.java
+++ app/src/main/java/com/example/zufallsgenerator/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Mein erster Commit, Hallo Sepplschuss//
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
